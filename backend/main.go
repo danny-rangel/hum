f9695c6d6f3ed7837554e494de9de8445c4790df
Danny Rangel <danielrangel.tech@gmail.com>
2020-02-19 22:45:10 -0800

    adding gorilla mux router

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type user struct {
+	Name string `json:"name"`
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	user1 := user{Name: "Danny"}
+	userjson, err := json.Marshal(user1)
+
+	if err == nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(userjson)
+}
+
+func GetPosts(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
-	fmt.Println("Hum")
+	r := mux.NewRouter()
+	r.HandleFunc("/user/:id", GetUser)
+	r.HandleFunc("/posts/:id", GetPosts)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
