5130db5a8f98a2b9e0571cb40f3d1bc172bb0d15
Danny Rangel <danielrangel.tech@gmail.com>
2020-02-19 23:01:21 -0800

    error fix and params test

--- backend/main.go
+++ backend/main.go
@@ -13,10 +13,11 @@ type user struct {
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	user1 := user{Name: "Danny"}
+	params := mux.Vars(r)
+	user1 := user{Name: params["name"]}
 	userjson, err := json.Marshal(user1)
 
-	if err == nil {
+	if err != nil {
 		panic(err)
 	}
 
@@ -26,13 +27,36 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetPosts(w http.ResponseWriter, r *http.Request) {
+	user1 := user{Name: "Danny"}
+	userjson, err := json.Marshal(user1)
 
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(userjson)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	user1 := user{Name: "Danny"}
+	userjson, err := json.Marshal(user1)
+
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(userjson)
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/user/:id", GetUser)
-	r.HandleFunc("/posts/:id", GetPosts)
+	r.HandleFunc("/", Index)
+	r.HandleFunc("/user/{name}", GetUser)
+	r.HandleFunc("/posts/{id}", GetPosts)
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
